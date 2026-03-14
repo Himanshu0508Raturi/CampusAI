0daa56810feb273a59d9628f40a32651dbe45e8c
Himanshu Raturi <raturihimanshu077@gmail.com>
2026-03-15 00:23:29 +0530

    Add CORS middleware to FastAPI application

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from schema import QueryRequest , QueryResponse , AgentState
 from agentic_rag import ask_question
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="CampusAI API",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/home")
